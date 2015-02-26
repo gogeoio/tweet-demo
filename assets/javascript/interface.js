daaa2f596401531f4d825e970a21ec229d7b1601
Tiago Oliveira <lokidg@gmail.com>
2015-02-26 08:57:01 -0300

    tweet-demo
    ajustes no javascript! Depois precisamos limpar os assets
    desnecessários.

--- assets/javascript/interface.js
+++ assets/javascript/interface.js
@@ -3,3 +3,24 @@ $('#rangedate .input-daterange').datepicker({
     multidate: true,
     todayHighlight: true
 });
+
+// Instance the tour
+var tour = new Tour({
+  steps: [
+  {
+    element: "#map",
+    title: "Title of my step",
+    content: "Content of my step"
+  },
+  {
+    element: "#sidebar-dashboard",
+    title: "Title of my step",
+    content: "Content of my step"
+  }
+]});
+
+// Initialize the tour
+tour.init();
+
+// Start the tour
+tour.start();
