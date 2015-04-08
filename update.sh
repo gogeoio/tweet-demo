1d1b37263f43e6bbc00f31083bcc365b3b5238e8
Roberto Rodrigues Junior <robertogyn19@gmail.com>
2015-04-08 17:12:55 -0300

    Update update.sh script

--- update.sh
+++ update.sh
@@ -4,12 +4,14 @@ git checkout -- .
 git pull origin master
 
 APP_FOLDER=gogeo-tweet
-
 cd $APP_FOLDER
+
+rm -rf dist-old
+mv dist-prod dist-old
+
 bower install
 gulp deploy
-cd ..
 
-rm -rf $APP_FOLDER/dist-old
-mv $APP_FOLDER/dist $APP_FOLDER/dist-old
-mv $APP_FOLDER/dist $APP_FOLDER/dist-prod
+mv dist dist-prod
+
+cd ..
