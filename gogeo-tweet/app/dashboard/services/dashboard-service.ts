cbc9ba3095e7162e3c72b96b2f11673970591a90
Roberto Rodrigues Junior <robertogyn19@gmail.com>
2015-12-03 08:49:11 -0200

    Fix popup click

--- gogeo-tweet/app/dashboard/services/dashboard-service.ts
+++ gogeo-tweet/app/dashboard/services/dashboard-service.ts
@@ -366,7 +366,7 @@ module gogeo {
 
         private getTweetData(latlng: L.LatLng, zoom: number, thematicQuery?: ThematicQuery) {
             var url = Configuration.makeUrl("geosearch/db1/" + Configuration.getCollectionName() + "?mapkey=123");
-            var pixelDist = 2575 * Math.cos((latlng.lat * Math.PI / 180)) / Math.pow(2, (zoom + 8));
+            var pixelDist = 40075 * Math.cos((latlng.lat * Math.PI / 180)) / Math.pow(2, (zoom + 8)) * 8;
             var query = this.composeQuery().requestData.q;
 
             if (thematicQuery) {
